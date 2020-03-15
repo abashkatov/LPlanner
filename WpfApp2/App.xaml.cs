554349ae9a46069ff94ca5a91ee55aefa4009316
abashkatov <as@bashkatov.pro>
2020-03-15 16:52:17 +0300

    Добавляет загрузку конфига из .ini-файла.

--- WpfApp2/App.xaml.cs
+++ WpfApp2/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,10 +16,20 @@ namespace WpfApp2
     /// </summary>
     public partial class App : Application
     {
+        private const string ConfigPath = "config.ini";
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             #region Настройка контейнера
             IServiceCollection services = new ServiceCollection();
+
+            if (!File.Exists(ConfigPath))
+            {
+                using (File.Create(ConfigPath)) { }
+            }
+            var configBuilder = new ConfigurationBuilder().AddIniFile(ConfigPath);
+            IConfiguration config = configBuilder.Build();
+            services.AddSingleton<IConfiguration>(config);
+
             services.AddTransient<MainWindow, MainWindow>();
             #endregion
 
