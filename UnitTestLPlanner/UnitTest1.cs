c6179056423644fc017bd27e5b624a6f3041981c
abashkatov <as@bashkatov.pro>
2020-03-16 00:31:23 +0300

    Добавляет болванку проекта с успешным тестом..

---
+++ UnitTestLPlanner/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestLPlanner
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreSame(1, 1);
+        }
+    }
+}
